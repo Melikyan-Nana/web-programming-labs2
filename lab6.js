ccc466cb5dad8f57fdb58f57f141414128e40a12
Melikyan Nakhshun <melikyan@example.com>
2022-11-30 10:44:23 +0700

    Переводы

---
+++ lab6.js
@@ -0,0 +1,43 @@
+function setRussian(){
+    let name = document.getElementById('student');
+    name.innerHTML = 'Нахшун Меликян';
+
+   let footer = document.getElementById('footer');
+   footer.innerHTML = 'Факультут бизнеса, ФБИ-01, 3 курс, 2022';
+
+   document.getElementById('your-name').innerHTML = 'Ваше имя';
+   document.getElementById('input-name').placeholder= 'введите имя';
+
+   document.getElementById('your-surname').innerHTML = 'Ваша фамилия';
+   document.getElementById('input-surname').placeholder= 'введите фамилию';
+
+   document.getElementById('your-age').innerHTML = 'Ваш возраст';
+   document.getElementById('input-age').placeholder= 'введите возраст';
+
+   document.getElementById('ok-button').value = 'ок';
+
+   document.getElementById('reset-button').value= 'сброс';
+}
+
+function setEnglish() {
+    let name = document.getElementById('student');
+    name.innerHTML = 'Nakhshun Melikyan';
+
+    let footer = document.getElementById('footer');
+   footer.innerHTML = 'Faculty of Business, FBI-01, 3rd year, 2022';
+
+   document.getElementById('your-name').innerHTML = 'Your name';
+   document.getElementById('input-name').placeholder = 'enter your name';
+
+   document.getElementById('your-surname').innerHTML = 'Your surname';
+   document.getElementById('input-surname').placeholder= 'enter your surname';
+
+   document.getElementById('your-age').innerHTML = 'Your age';
+   document.getElementById('input-age').placeholder= 'enter your age';
+
+   document.getElementById('ok-button').value = 'ok';
+
+   document.getElementById('reset-button').value= 'reset';
+}
+
+
