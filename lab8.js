4866b71fb4bb4db21347192e1b133dcae5520499
Melikyan Nakhshun <melikyan@example.com>
2022-12-14 10:55:15 +0700

    Время

--- lab8.js
+++ lab8.js
@@ -1,11 +1,31 @@
 function showDate(params) {
     let out = document.getElementById('today');
     let today = new Date ();
-    out.innerHTML = today.toLocaleString('ru-RU')
-    new Date().toTimeString()
+    out.innerHTML = today.toLocaleDateString('ru-RU')
+}
+
+function showDaysCount(params){
+    let today = new Date ();
+    let inputDate = document.querySelector('input[type=date]');
+    let birthday = new Date (inputDate.value);
+    let daysCount = (today - birthday)/1000/60/60/24;
+    let result = document.getElementById('result');
+    daysCount = Math.ceil(daysCount); // округляем в меньшую сторону
+    result.innerHTML = 'С даты рождения прошло дней:' + daysCount;
+   
 }
 
 window.addEventListener('load', showDate);
+
+function showTime(){
+    let outTime = document.getElementById('time');
+    let currentTime = new Date();
+    outTime.innerHTML = currentTime.toLocaleTimeString('ru');
+}
+
+window.addEventListener('load', showTime);
+setInterval(showTime, 1000);
+
 
 
 
