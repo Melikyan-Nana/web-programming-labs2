af99544d2c61a4c877f37a08718368ad468756a1
Меликян Нахшун <melikyan-nana@mail.ru>
2022-12-23 11:57:43 +0700

    Фокус

--- lab9.js
+++ lab9.js
@@ -40,5 +40,21 @@ window.addEventListener('DOMContentLoaded',function(){
     coords.addEventListener('mousemove', showCoords); 
 });
 
-
+function showFocus(event){
+    event.target.style.color = 'green';
+    event.target.style.size = '15px';
+}
+function showBlur(event){
+    event.target.style.color = 'black';
+    event.target.style.size = '25px';
+}
+window.addEventListener('DOMContentLoaded', function(){
+    let texts = document.querySelectorAll('input[type="text"]');
+    texts[0].addEventListener('focus', showFocus);
+    texts[1].addEventListener('focus', showFocus);
+    texts[2].addEventListener('focus', showFocus);
+    texts[0].addEventListener('blur', showBlur);
+    texts[1].addEventListener('blur', showBlur);
+    texts[2].addEventListener('blur', showBlur);
+});
 
