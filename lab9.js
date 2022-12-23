19695dccd0e60433c6c8f5749df9e38502d9f3f5
Меликян Нахшун <melikyan-nana@mail.ru>
2022-12-23 10:13:40 +0700

    Переключения состояний

--- lab9.js
+++ lab9.js
@@ -10,3 +10,24 @@ function clickNameSurname(button) {
     else
     button.innerHTML = 'Нахшун';
 }
+function clickCondition(button) {
+    if (button.innerHTML =='Нажми меня') {
+        button.innerHTML ='Ещё раз нажми';
+         button.style.color = 'orange'
+    }
+    else if (button.innerHTML =='Ещё раз нажми') {
+        button.innerHTML = 'Ещё';
+        button.style.color = 'red'
+    }
+       
+    else if (button.innerHTML == 'Ещё') {
+        button.innerHTML = 'Начать сначала';
+        button.style.color = 'green'
+    }
+      
+    else{
+        button.innerHTML = 'Нажми меня';
+        button.style.color = 'blue'
+    }
+}
+    
